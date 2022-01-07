3dbaf6b9cb15c90a1ce84e80721eabcb5f514882
zataara <bryantmac@gmail.com>
2022-01-06 23:13:53 -0800

    added print statements for seeding status

--- seed.py
+++ seed.py
@@ -10,11 +10,16 @@ db.create_all()
 
 with open('generator/users.csv') as users:
     db.session.bulk_insert_mappings(User, DictReader(users))
+    print("Inserted Users")
 
 with open('generator/messages.csv') as messages:
     db.session.bulk_insert_mappings(Message, DictReader(messages))
+    print("Inserted Messages")
 
 with open('generator/follows.csv') as follows:
     db.session.bulk_insert_mappings(Follows, DictReader(follows))
+    print("Inserted Follows")
+    
+print("Seeding Complete")
 
 db.session.commit()
